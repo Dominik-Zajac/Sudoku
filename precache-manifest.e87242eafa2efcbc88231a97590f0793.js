27e5d4b0778aacca3cf449bdacd78c273f4fa29c
Wyrafinowany <zajac_dominik@onet.pl>
2019-03-24 18:48:52 +0100

    Updates

---
+++ precache-manifest.e87242eafa2efcbc88231a97590f0793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cffc9867b29c5e1030a1",
+    "url": "/Sudoku3/static/js/runtime~main.b95f4e1a.js"
+  },
+  {
+    "revision": "81f14abd86fce00421ef",
+    "url": "/Sudoku3/static/js/main.b67d8db6.chunk.js"
+  },
+  {
+    "revision": "6848d93d10690586f93f",
+    "url": "/Sudoku3/static/js/2.873a6f36.chunk.js"
+  },
+  {
+    "revision": "81f14abd86fce00421ef",
+    "url": "/Sudoku3/static/css/main.29f0149a.chunk.css"
+  },
+  {
+    "revision": "0e690d6aff03d5b0352d6baba0593118",
+    "url": "/Sudoku3/index.html"
+  }
+];
